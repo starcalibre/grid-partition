6d1e1d634859171a2be088aa039cf79cf34fdf2e
Adrian Hecker <starcalibre@gmail.com>
2017-02-03 10:45:07 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+2D Spatial Partition in for JavaScript. Watch this space.
---
+++ src/spatialPartition.js
@@ -0,0 +1,27 @@
+'use strict';
+
+function spatialPartition(width, height, cellWidth, cellHeight) {
+
+}
+
+spatialPartition.prototype.add = function(entity) {
+    // add a single entity
+}
+
+spatialPartition.prototype.addAll = function(entities) {
+    // add all entities
+}
+
+spatialPartition.prototype.move = function(entity, x, y) {
+    // move an entity
+}
+
+spatialPartition.prototype.x = function() {
+    // set the x-dimension accessor
+}
+
+spatialPartition.prototype.y = function() {
+    // set the y-dimension getter
+}
+
+module.exports = spatialPartition;

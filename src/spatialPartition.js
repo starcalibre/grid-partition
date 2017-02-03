3b31f4210ba5f86df92d43cdc80f46ffdd8251b6
Adrian Hecker <starcalibre@gmail.com>
2017-02-03 14:21:41 +1100

    Add add method and fix constructor * Include unit tests for add method * Fix stupid typo in constructor * Add tests to make sure custom grid sizes behave as they should

--- src/spatialPartition.js
+++ src/spatialPartition.js
@@ -1,12 +1,12 @@
 'use strict';
 
 function SpatialPartition(width, height, numberCellsX, numberCellsY) {
-    this.width = width | 100;
-    this.height = height | 100;
-    this.numberCellsX = numberCellsX | 10;
-    this.numberCellsY = numberCellsY | 10;
+    this.width = width || 100;
+    this.height = height || 100;
+    this.numberCellsX = numberCellsX || 10;
+    this.numberCellsY = numberCellsY || 10;
     this.cellWidth = this.width / this.numberCellsX;
-    this.cellHeight = this.width / this.numberCellsY;
+    this.cellHeight = this.height / this.numberCellsY;
 
     // default x-y accessors
     this._x = function(d) {
@@ -31,21 +31,32 @@ function SpatialPartition(width, height, numberCellsX, numberCellsY) {
 }
 
 SpatialPartition.prototype.add = function(entity) {
+    var posX = Math.round(this._x.call(null, entity));
+    var posY = Math.round(this._y.call(null, entity));
+    var cellX = Math.floor(posX / this.cellWidth);
+    var cellY = Math.floor(posY / this.cellHeight);
 
+    this._cells[cellY][cellX].push(entity);
+    this._entityMap[entity] = [cellX, cellY];
 };
 
 SpatialPartition.prototype.addAll = function(entities) {
 };
 
 SpatialPartition.prototype.getCell = function(cellX, cellY) {
-    // get cell
+    // assume length-2 array was passed
+    if(!cellY) {
+        return this._cells[cellX[1]][cellX[0]]
+    }
+    // otherwise assume individual accessors were passed
+    return this._cells[cellY][cellX];
 };
 
 SpatialPartition.prototype.move = function(entity, x, y) {
     // move an entity
 };
 
-SpatialPartition.prototype.remove = function(entity, x, y) {
+SpatialPartition.prototype.remove = function(entity) {
     // remove an entity
 };
 
--- test/spatialPartition.spec.js
+++ test/spatialPartition.spec.js
@@ -21,16 +21,112 @@ describe('SpatialPartition', function() {
             assert.strictEqual(actual, expected);
         });
 
-        it('should have default cell width 10', function() {
+        it('should have default number of X cells 10', function() {
             var actual = grid.cellWidth;
             var expected = 10;
             assert.strictEqual(actual, expected);
         });
 
-        it('should have default cell height 10', function() {
+        it('should have default number of Y cells 10', function() {
             var actual = grid.cellHeight;
             var expected = 10;
             assert.strictEqual(actual, expected);
+        });
+
+        it('should have expected cell width for custom parameters', function() {
+            var testGrid = new SpatialPartition(90, 80, 3, 4);
+            var expected = 30;
+            var actual = testGrid.cellWidth;
+            assert.strictEqual(actual, expected);
+        });
+
+        it('should have expected cell height for custom parameters', function() {
+            var testGrid = new SpatialPartition(90, 80, 3, 4);
+            var expected = 20;
+            var actual = testGrid.cellHeight;
+            assert.strictEqual(actual, expected);
+        });
+    });
+    
+    describe('add', function() {
+        it('should add the new entity to the expected cell', function() {
+            // test entity, on a 100x100 space partitioned unto 10x10 cells,
+            // the entity will live at the grid [0, 5]
+            var grid = new SpatialPartition();
+
+            var testEntity = {
+                name: 'Alice The Entity',
+                x: 0,
+                y: 51
+            };
+
+            grid.add(testEntity);
+            var cell = grid.getCell(0, 5);
+            var actual = cell[0];
+            assert.equal(actual, testEntity);
+        });
+
+        it('should add the new entity to expected cell with non-default params', function() {
+            // create a 90 * 80 space partitioned 3 * 4 grids
+            var grid = new SpatialPartition(90, 80, 3, 4);
+
+            // the entity should live at [1, 3]
+            var testEntity = {
+                name: 'Charlie The Entity',
+                x: 35,
+                y: 70
+            };
+
+            grid.add(testEntity);
+            var cell = grid.getCell(1, 3);
+
+            var actual = cell[0];
+            assert.equal(actual, testEntity);
+        });
+
+        it('addition works custom x, y accessors', function() {
+            var grid = new SpatialPartition()
+                .x(function(d) {
+                    return d.crazy.nesting.what;
+                })
+                .y(function(d) {
+                    return d.why.would.you.why * 10;
+                });
+
+            // expected this to map to
+            // [25, 4.5 * 10] == [25, 45] -> [2, 4]
+            var testEntity = {
+                name: 'Sally The Entity With Crazy Properties',
+                crazy: { nesting: { what: 25} },
+                why: { would: { you: { why: 4.5 } } }
+            };
+
+            grid.add(testEntity);
+            var cell = grid.getCell(2, 4);
+
+            var actual = cell[0];
+            assert.equal(actual, testEntity);
+        });
+
+        it('updates the entity map as expected', function() {
+            var grid = new SpatialPartition()
+                .x(function(d) {
+                    return d.position.x;
+                })
+                .y(function(d) {
+                    return d.position.y;
+                });
+
+            // expect the entity to [7, 7]
+            var testEntity = {
+                name: 'Patel The Entity',
+                position: { x: 70, y: 70 }
+            };
+
+            grid.add(testEntity);
+            var actual = grid._entityMap[testEntity];
+            var expected = [7, 7];
+            assert.deepEqual(actual, expected);
         });
     });
 });

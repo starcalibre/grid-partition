8b6ecfb97896fa27391331c936485fd7fb231969
Adrian Hecker <starcalibre@gmail.com>
2017-02-14 14:50:03 +1100

    Add JSDoc strings for all functions

--- src/spatialPartition.js
+++ src/spatialPartition.js
@@ -3,6 +3,15 @@ var Map = require('collections/weak-map');
 var isNullOrUndefined = require('./util').isNullOrUndefined;
 var mod = require('./util').mod;
 
+/**
+ * Creates a new SpatialPartition object.
+ *
+ * @param {number} width - The width of the world-space.
+ * @param {number} height - The height of the world-space.
+ * @param numberCellsX - The number of cells to divide the x-dimension to.
+ * @param numberCellsY - The number of cells to divide the y-dimension to.
+ * @constructor
+ */
 function SpatialPartition(width, height, numberCellsX, numberCellsY) {
     this.width = width || 100;
     this.height = height || 100;
@@ -35,6 +44,11 @@ function SpatialPartition(width, height, numberCellsX, numberCellsY) {
     this._entityMap = new Map();
 }
 
+/**
+ * Adds an entity to the grid.
+ *
+ * @param {Object} entity - The entity to add to the grid.
+ */
 SpatialPartition.prototype.add = function(entity) {
     var posX = Math.floor(this._x.call(null, entity));
     var posY = Math.floor(this._y.call(null, entity));
@@ -45,24 +59,57 @@ SpatialPartition.prototype.add = function(entity) {
     this._entityMap.set(entity, [cellX, cellY]);
 };
 
+/**
+ * Adds multiple entities to the grid.
+ *
+ * @param {Array} entities - An array of entities to add to the grid.
+ */
 SpatialPartition.prototype.addAll = function(entities) {
     entities.forEach(function(entity) {
         this.add(entity);
     }.bind(this));
 };
 
+/**
+ * Gets an array of entities belonging to the [cellX, cellY] cell in
+ * the grid.
+ *
+ * @param {number} cellX - The grid's x-coordinate.
+ * @param {number} cellY - The grid's y-coordinate.
+ * @returns {Array} An array of entities belonging to this cell.
+ */
 SpatialPartition.prototype.getCell = function(cellX, cellY) {
     if(!this._isCellValid(cellX, cellY)) return null;
-    // otherwise assume individual accessors were passed
     return this._cells[cellY][cellX];
 };
 
+/**
+ * Gets an array of entities belonging to the same cell as the
+ * x and y position from the world-space. This maps the input
+ * world-space co-ordinate to the corresponding grid cell.
+ *
+ * @param {number} posX - The query x-position.
+ * @param {number} posY - The query y-position.
+ * @returns {Array} - An array of entities belonging to the same cell.
+ */
 SpatialPartition.prototype.getCellByWorldCoord = function(posX, posY) {
     var cellX = Math.floor(posX / this.cellWidth);
     var cellY = Math.floor(posY / this.cellHeight);
     return this.getCell(cellX, cellY);
 };
 
+/**
+ * Return the entities belonging to the cell at [cellX, cellY], and those belonging
+ * to the cells that are ``radius`` distance. The wrapping parameter determines whether or
+ * not the search should look on the opposite side of the grid space if the search radius
+ * falls "outside" the grid.
+ *
+ * @param {number} cellX - The grid's x-coordinate.
+ * @param {number} cellY - The grid's y-coordinate.
+ * @param {number} radius - The search radius.
+ * @param {boolean} wrap - Whether or not to wrap the search radius around the space.
+ * @returns {Array} - An array of entities belonging to the search neighbourhood. Default true.
+ */
 SpatialPartition.prototype.getNeighbourhood = function(cellX, cellY, radius, wrap) {
     // default parameters
     if(isNullOrUndefined(radius)) { radius = 1; }
@@ -88,12 +135,28 @@ SpatialPartition.prototype.getNeighbourhood = function(cellX, cellY, radius, wra
     return result;
 };
 
-SpatialPartition.prototype.getNeighbourhoodByWorldCoord = function(posX, posY, radius) {
+/**
+ * Given a world-space x, y co-ordinate, find all neighbours belonging to neighbouring grids.
+ * See getNeighbourhood.
+ *
+ * @param {number} posX - The grid's x-coordinate.
+ * @param {number} posY - The grid's y-coordinate.
+ * @param {number} radius - The search radius.
+ * @param {boolean} wrap - Whether or not to wrap the search radius around the space.
+ * @returns {Array} - An array of entities belonging to the search neighbourhood.
+ */
+SpatialPartition.prototype.getNeighbourhoodByWorldCoord = function(posX, posY, radius, wrap) {
     var cellX = Math.floor(Math.floor(posX) / this.cellWidth);
     var cellY = Math.floor(Math.floor(posY) / this.cellHeight);
-    return this.getNeighbourhood(cellX, cellY, radius);
+    return this.getNeighbourhood(cellX, cellY, radius, wrap);
 };
 
+/**
+ * Update the cell an entity belongs to if its world-space co-ordinates have changed.
+ *
+ * @param {Object} entity - The entity to update.
+ * @returns {boolean} - True if an update was successful.
+ */
 SpatialPartition.prototype.update = function(entity) {
     // remove the entity
     if(!this.remove(entity)) return false;
@@ -103,12 +166,23 @@ SpatialPartition.prototype.update = function(entity) {
     return true;
 };
 
+/**
+ * Update the cells for multiple entities.
+ *
+ * @param {Array} entities - An array of entities to update.
+ */
 SpatialPartition.prototype.updateAll = function(entities) {
     entities.forEach(function(entity) {
         this.update(entity);
     }.bind(this));
 };
 
+/**
+ * Remove an entity from the grid.
+ *
+ * @param entity
+ * @returns {boolean}
+ */
 SpatialPartition.prototype.remove = function(entity) {
     var cellCoord = this._entityMap.get(entity);
     if(isNullOrUndefined(cellCoord)) return false;
@@ -124,6 +198,13 @@ SpatialPartition.prototype.remove = function(entity) {
     return true;
 };
 
+/**
+ * Set a custom x coordinate accessor for the entities.
+ * This defaults to the x property of the entity.
+ *
+ * @param {Function} _ - A function returning the x-coordinate for the entity.
+ * @returns {SpatialPartition} - Returns this SpatialPartition.
+ */
 SpatialPartition.prototype.x = function(_) {
     if(!isNullOrUndefined(_)) {
         this._x = _;
@@ -132,6 +213,13 @@ SpatialPartition.prototype.x = function(_) {
     return this._x;
 };
 
+/**
+ * Set a custom y coordinate accessor for the entities.
+ * This defaults to the y property of the entity.
+ *
+ * @param {Function} _ - A function returning the y-coordinate for the entity.
+ * @returns {SpatialPartition} - Returns this SpatialPartition.
+ */
 SpatialPartition.prototype.y = function(_) {
     if(!isNullOrUndefined(_)) {
         this._y = _;
@@ -140,6 +228,14 @@ SpatialPartition.prototype.y = function(_) {
     return this._y;
 };
 
+/**
+ * Check if the given cellX and cellY co-ordinates are valid for this grid.
+ *
+ * @param {number} cellX - The query x coordinate.
+ * @param {number} cellY - The query y coordinate.
+ * @returns {boolean} - True if valid, false otherwise.
+ * @private
+ */
 SpatialPartition.prototype._isCellValid = function(cellX, cellY) {
     return (cellX >= 0 && cellY >= 0 && cellX < this.numberCellsX && cellY < this.numberCellsY);
 };
--- src/util.js
+++ src/util.js
@@ -1,9 +1,24 @@
 'use strict';
 
 module.exports = {
+    /**
+     * Check if a given value of null or undefined.
+     *
+     * @param {*} value - The query value.
+     * @returns {boolean} - Whether or not the given value is null or undefined.
+     */
     isNullOrUndefined: function(value) {
         return value === null || value === undefined;
     },
+
+    /**
+     * Find the modulo m for number n. This is used in place of the JS % operator as it behaves
+     * as one would with negative numbers expect, while the default JS implementation.
+     *
+     * @param {number} n - The number n.
+     * @param {number} m - Modulo m.
+     * @returns {number} - The result of of n mod m.
+     */
     mod: function(n, m) {
         return ((n % m) + m) % m;
     }
